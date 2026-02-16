825f3c6f1dc9c6fdfe51841fdcd37f3346274714
PrunesLand <panargya@gmail.com>
2026-02-16 18:16:43 +0100

    feat: Created more features to train models

--- src/analyze.py
+++ src/analyze.py
@@ -1,4 +1,6 @@
 from src.data_loader import download_data, load_data
+from src.features import extract_url_features
+import pandas as pd
 
 def main():
     path = download_data()
@@ -7,6 +9,12 @@ def main():
     print(df.shape)
     print(df.info())
     print(f"{df['type'].value_counts(normalize=True) * 100}")
+
+    X, y = extract_url_features(df)
+    
+    combined_df = pd.concat([X, y], axis=1)
+    print(combined_df.head())
+    
 
 if __name__ == "__main__":
     main()
