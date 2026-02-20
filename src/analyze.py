ed827231f70e8c7dcfee94185a7a28fb94c23aff
PrunesLand <panargya@gmail.com>
2026-02-20 18:54:23 +0100

    chore: improve readablility of analysis file

--- src/analyze.py
+++ src/analyze.py
@@ -5,15 +5,19 @@ import pandas as pd
 def main():
     path = download_data()
     df = load_data(path)
+    print("\n === Head ===\n")
     print(df.head())
+    print("\n === Shape ===\n")
     print(df.shape)
+    print("\n === Info ===\n")
     print(df.info())
+    print("\n === Value Counts ===\n")
     print(f"{df['type'].value_counts(normalize=True) * 100}")
-
+    print("\n === Post Feature Engineering ===\n")
     X, y = extract_url_features(df)
     
     combined_df = pd.concat([X, y], axis=1)
-    print(combined_df.head())
+    print(combined_df.info())
     
 
 if __name__ == "__main__":
