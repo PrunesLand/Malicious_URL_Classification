ee6cc8f451eeead8515a139d6446f3651ace0c73
PrunesLand <panargya@gmail.com>
2026-02-21 09:20:31 +0100

    feat: optimize performance of models

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-from sklearn.svm._classes import SVC
+from sklearn.linear_model import SGDClassifier
 from xgboost import XGBClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
@@ -6,10 +6,10 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.naive_bayes import GaussianNB
 
 def get_models():
-    clf1 = XGBClassifier()
-    clf2 = KNeighborsClassifier()
-    clf3 = MLPClassifier()
-    clf4 = SVC()
+    clf1 = XGBClassifier(n_jobs=-1)
+    clf2 = KNeighborsClassifier(n_jobs=-1)
+    clf3 = MLPClassifier(max_iter=400, verbose=True)
+    clf4 = SGDClassifier(loss='log_loss', n_jobs=-1, max_iter=1000)
     clf5 = GaussianNB()
     
     voting_clf = VotingClassifier(
@@ -17,18 +17,19 @@ def get_models():
             ('xgb', clf1), 
             ('knn', clf2), 
             ('mlp', clf3),
-            ('svm', clf4),
+            ('lsvm', clf4),
             ('gnb',clf5)
         ],
         voting='soft',
-        weights=[5, 2, 1, 3, 4]
+        weights=[5, 2, 1, 3, 4],
+        n_jobs=-1,
     )
 
     models = {
         'XGBClassifier': clf1,
         'KNeighborsClassifier': clf2,
         'MLPClassifier': clf3,
-        'SVM': clf4,
+        'LinearSVM': clf4,
         'GaussianNB': clf5,
         'WeightedVoting': voting_clf,
     }
